17736b599ec3ddc2b79371176526f13155df2ef7
vmanav <zmanav.1999@gmail.com>
2019-11-02 22:53:25 +0530

    Home page design in progress

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const app = express()
-0
-app.use('/', express.static(__dirname + 'public'))
+const app = express();
+
+app.use('/', express.static(__dirname + '/public'))
+app.use('/scripts', express.static(__dirname + '/scripts'))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.send("Blog It Out REBORN")
+    // res.send("Blog It Out REBORN")
+
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    // res.send("Blog It Out REBORN")
+
+    res.render('about')
 })
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log("Application running on :http://localhost:3000/")
+    console.log("Application running on : http://localhost:3000/")
 })
---
+++ scripts/indexScript.js
@@ -0,0 +1,20 @@
+$(() => {
+
+    window.onscroll = function () { scrollFunction() };
+    navbarHeight = $("#myNavbar").height();
+    console.log("Height of navbar :", navbarHeight)
+
+    function scrollFunction() {
+
+        var x = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        if (document.body.scrollTop > (x - navbarHeight) || document.documentElement.scrollTop > (x - navbarHeight)) {
+            document.getElementById("myNavbar").classList.add("bg-dark", "navbar-dark");
+            $("#goToTopButton").addClass("goToTopButton-visible");
+        }
+        else {
+            document.getElementById("myNavbar").classList.remove("bg-dark", "navbar-dark");
+            $("#goToTopButton").removeClass("goToTopButton-visible");
+        }
+    }
+
+})
