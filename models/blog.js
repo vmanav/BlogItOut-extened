1cf4a5a411056b30ac2df2ae11e091be78e65e4b
vmanav <zmanav.1999@gmail.com>
2019-12-20 20:58:26 +0530

    basic blog Schema done, now design the dashboard

---
+++ models/blog.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+// Creating blogSchema
+const blogSchema = new mongoose.Schema({
+    blogTitle: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    blogBody: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    blogTags: {
+        type: String,
+        trim: true
+    },
+    likesArray: {
+        type: Array
+    },
+    likesCount: {
+        type: Number,
+    }
+});
+
+// Creating Mongoose Model from blogSchema
+var Blog = mongoose.model('Blog', blogSchema, 'blogs');// 'blogs' -> Collection Name
+
+module.exports = {
+    Blog
+}
