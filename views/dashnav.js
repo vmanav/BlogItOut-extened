6c36e56fd49ceabe872b3f2f52ef1a84d6ef19b4
vmanav <zmanav.1999@gmail.com>
2019-12-26 23:22:23 +0530

    dashboard navbar setu finnally now moving to backend

--- views/dashnav.js
+++ views/dashnav.js
@@ -6,12 +6,33 @@ $(() => {
     // $(".collapse").on('show.bs.collapse', function () {
     //     alert('The collapsible content is about to be shown.');
     // });
+    (function () {
+        // $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="tooltip"]').tooltip('enable')
 
+    }
+    );
 
+    $('.nav-item').hover(
+        function () { $(this).addClass('active') },
+        function () { $(this).removeClass('active') }
+    )
+
+    set = false;
     $('.toggleButtons').on('click', function () {
+        if (set == false) {
+            $('[data-toggle="tooltip"]').tooltip('disable')
+            set = true
+        }
+        else {
+            $('[data-toggle="tooltip"]').tooltip('enable')
+            set = false;
+        }
+        // Disable tooltips
+        // $('[data-toggle="tooltip"]').tooltip('enable')
 
         $("#content").toggleClass("grayed");
-        
+        $(".nav-link").toggleClass("text-center");
         $(".toggleButtons").toggleClass("hiddenButtons");
 
     });
