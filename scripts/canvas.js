ce6e374fa267754c78690781673c9a2e7ec2d4d4
vmanav <zmanav.1999@gmail.com>
2020-01-15 19:29:07 +0530

    Added Logo Icons, Perfected Login Page, NEXT STEP - Navbar.

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -24,7 +24,7 @@ $(() => {
     window.addEventListener("mousemove", function (event) {
         mouse.x = event.x;
         mouse.y = event.y;
-        console.log(mouse);
+        // console.log(mouse);
     })
 
 
